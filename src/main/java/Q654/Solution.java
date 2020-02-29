71ae113b6223a44a25612e04bd94b9ac0b3e24a1
Fangfang <wangyifang@outlook.com>
2020-02-29 21:26:41 +0000

    Q654

--- src/main/java/Q654/Solution.java
+++ src/main/java/Q654/Solution.java
@@ -1,7 +1,31 @@
 package Q654;
 
-class Solution {
-    public TreeNode constructMaximumBinaryTree(int[] nums) {
+import java.util.Arrays;
 
+public class Solution {
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums.length == 0 ) {
+            return null;
+        } else if (nums.length == 1) {
+            return new TreeNode(nums[0]);
+        } else {
+            int maxNum = Arrays.stream(nums).max().getAsInt();
+            int maxIndex = getIndexOfLargest(nums);
+            int[] leftNode = Arrays.copyOfRange(nums, 0, maxIndex);
+            int[] rightNode = Arrays.copyOfRange(nums, maxIndex+1, nums.length);
+            TreeNode result = new TreeNode(maxNum);
+            result.left = constructMaximumBinaryTree(leftNode);
+            result.right = constructMaximumBinaryTree(rightNode);
+            return result;
+        }
+    }
+
+    private int getIndexOfLargest( int[] array ){
+        int largest = 0;
+        for ( int i = 1; i < array.length; i++ ) {
+            if ( array[i] > array[largest] ) largest = i;
+        }
+        return largest;
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,6 @@
 public class main {
     public static void main(String arg[]) {
-        String result = Solution.multiply("99", "0");
-        System.out.print(result);
+
     }
 
 
