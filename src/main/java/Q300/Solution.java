039d9970f86ca36fcd3487c4f571d32ea7e9a2ca
Fangfang <wangyifang@outlook.com>
2020-03-05 23:21:25 +0000

    Q300

---
+++ src/main/java/Q300/Solution.java
@@ -0,0 +1,24 @@
+package Q300;
+
+import static java.lang.Math.*;
+
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        int globalMax = 1;
+        for (int i=1; i<nums.length; i++) {
+            int localMax = Integer.MIN_VALUE;
+            for (int j=i-1; j>=0; j--) {
+                if (nums[j] < nums[i]) {
+                    localMax = max(localMax, result[j] + 1);
+                }
+
+            }
+            result[i] = localMax == Integer.MIN_VALUE? 1 : localMax;
+            globalMax = max(globalMax, result[i]);
+        }
+        return globalMax;
+    }
+}
