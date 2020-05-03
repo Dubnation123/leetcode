09b79a2fded6c3b2e61f8583de573cb1c603b534
Fangfang <wangyifang@outlook.com>
2020-05-03 09:29:41 +0100

    Q5

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,6 +3,5 @@ import Q5.Solution;
 public class main {
     public static void main(String arg[]) {
         Solution solution = new Solution();
-        String result = solution.longestPalindrome("abacdgfdcaba");
     }
 }
