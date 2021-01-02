aa4c6429a6f2544dda6234241f409780a55745b6
Fangfang <wangyifang@outlook.com>
2021-01-02 17:12:36 +0000

    Q18

--- src/main/java/Q18/Solution.java
+++ src/main/java/Q18/Solution.java
@@ -1,4 +1,31 @@
 package Q18;
 
+import java.util.*;
+
 public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i] != nums[i-1]) {
+                for (int j = i+1; j < nums.length; j++) {
+                    if (j == i+1 || nums[j] != nums[j-1]) {
+                        helper(nums, i, j, target, result);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private void helper(int[] nums, int index1, int index2,int target, List<List<Integer>> result) {
+        Set<Integer> seen = new HashSet<>();
+        for (int i = index2+1; i < nums.length ; i++) {
+            if (seen.contains(target - nums[index1] - nums[index2]- nums[i])) {
+                result.add(List.of(nums[index1], nums[index2], nums[i], target - nums[index1] - nums[index2]- nums[i]));
+                while (i < nums.length-1 && nums[i] == nums[i+1]) i++;
+            }
+            seen.add(nums[i]);
+        }
+    }
 }
---
+++ src/main/java/Q224/Solution.java
@@ -0,0 +1,5 @@
+package Q224;
+
+public class Solution {
+
+}
